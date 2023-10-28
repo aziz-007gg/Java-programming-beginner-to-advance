32b8c12b714b78696f3a638d8c8bbc117c553b38
Aziz Safin <zzshafin@gmail.com>
2023-10-28 14:17:20 +0600

    java file

---
+++ a87_Conversion_between_String_and_Primitive_Data_type.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.java;
+
+/**
+ *
+ * @author zzsha
+ */
+public class a87_Conversion_between_String_and_Primitive_Data_type {
+    public static void main(String[] args) {
+        int i=10;
+        String s=Integer.toString(i);
+        System.out.println(s);
+        
+        String b="10";
+        int x=Integer.parseInt(b);
+        System.out.println(x);
+        
+    }
+    
+}
